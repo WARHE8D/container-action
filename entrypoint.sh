f9e2d2e50d8b369d8993998b23ed2577b12cf7e9
Murtuza <59991459+WARHE8D@users.noreply.github.com>
2026-02-15 03:14:37 -0700

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -l
+
+result=$(($1 + $2))
+
+echo "result=$result" >> $GITHUB_OUTPUT
+
+echo "output file" > containerOutput.txt
+
+# The entrypoint.sh file is created to handle the inputs and outputs for the Docker container action. 
+# It grabs the inputs from the metadata file, echoes the output to the GitHub output environment file, 
+# and creates a file that can be accessed through the GitHub workspace. This script is essential for defining the behavior of the Docker container action.
